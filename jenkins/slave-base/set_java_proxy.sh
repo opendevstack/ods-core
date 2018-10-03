88987d41062147b81b4e408351e6cde05d3905b5
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2018-10-03 18:27:31 +0200

    cap d

--- jenkins/slave-base/set_java_proxy.sh
+++ jenkins/slave-base/set_java_proxy.sh
@@ -4,21 +4,21 @@
 if [[ $HTTP_PROXY != "" ]]; then
 
 	proxy=$(echo $HTTP_PROXY | sed -e "s|https://||g" | sed -e "s|http://||g")
-	proxy_hostp=$(echo $proxy | cut -d "@" -f2)
+	proxy_hostp=$(echo $proxy | cut -D "@" -f2)
 	
-	proxy_host=$(echo $proxy_hostp | cut -d ":" -f1)
-	JAVA_OPTS="$JAVA_OPTS -dhttp.proxyHost=$proxy_host -dhttps.proxyHost=$proxy_host"
+	proxy_host=$(echo $proxy_hostp | cut -D ":" -f1)
+	JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyHost=$proxy_host -Dhttps.proxyHost=$proxy_host"
 	
-	proxy_port=$(echo $proxy_hostp | cut -d ":" -f2)
-	JAVA_OPTS="$JAVA_OPTS -dhttp.proxyPort=$proxy_port -dhttps.proxyPort=$proxy_port"
+	proxy_port=$(echo $proxy_hostp | cut -D ":" -f2)
+	JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyPort=$proxy_port -Dhttps.proxyPort=$proxy_port"
 
-	proxy_userp=$(echo $proxy | cut -d "@" -f1)
+	proxy_userp=$(echo $proxy | cut -D "@" -f1)
 	if [[ $proxy_userp != $proxy_hostp ]]; 
 	then
-		proxy_user=$(echo $proxy_userp | cut -d ":" -f1)
-		JAVA_OPTS="$JAVA_OPTS -dhttp.proxyUser=$proxy_user -dhttps.proxyUser=$proxy_user"
+		proxy_user=$(echo $proxy_userp | cut -D ":" -f1)
+		JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyUser=$proxy_user -Dhttps.proxyUser=$proxy_user"
 		proxy_pw=$(echo $proxy_userp | sed -e "s|$proxy_user:||g")
-		JAVA_OPTS="$JAVA_OPTS -dhttp.proxyPassword=$proxy_pw -dhttps.proxyPassword=$proxy_pw"
+		JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyPassword=$proxy_pw -Dhttps.proxyPassword=$proxy_pw"
  	fi
  	export JAVA_OPTS=$JAVA_OPTS;
 fi
