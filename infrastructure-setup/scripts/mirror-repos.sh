6261e024d52490c5bcd812217419be13b765cc17
Zell, Volker <Volker.Zell@opitz-consulting.com>
2018-11-21 11:33:07 +0100

    typo and shebang fix

--- infrastructure-setup/scripts/mirror-repos.sh
+++ infrastructure-setup/scripts/mirror-repos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This scripts mirros the core ods repos into your own Bitbucket server.
+# This scripts mirrors the core ods repos into your own Bitbucket server.
 # By default, this is the Bitbucket server on your local machine as created by the getting started guide.
 # You can cutomize this by providing an environment variable: "REPO_TARGET_BASE" pointing to your git repo.
 # This includes everything before the project / repo part, this means everything before "/opendevstack/..."
@@ -20,13 +20,13 @@ git clone --bare https://github.com/opendevstack/ods-core.git
 git clone --bare https://github.com/opendevstack/ods-jenkins-shared-library.git
 git clone --bare https://github.com/opendevstack/ods-provisioning-app.git
 git clone --bare https://github.com/opendevstack/ods-project-quickstarters.git
-#!/bin/sh
+
 cd ods-configuration-sample.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-configuration-sample.git; cd -
 cd ods-core.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-core.git; cd -
 cd ods-jenkins-shared-library.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-jenkins-shared-library.git; cd -
 cd ods-project-quickstarters.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-project-quickstarters.git; cd -
 cd ods-provisioning-app.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-provisioning-app.git; cd -
-#!/bin/sh
+
 cd ods-configuration-sample.git; git push downstream; cd -
 cd ods-core.git; git push downstream; cd -
 cd ods-jenkins-shared-library.git; git push downstream; cd -
