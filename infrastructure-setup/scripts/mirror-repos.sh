dc7724e9de83c30cee858c63f10c776a1e8e003b
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2018-11-22 15:12:25 +0100

    - Implemented: changed mirror script

--- infrastructure-setup/scripts/mirror-repos.sh
+++ infrastructure-setup/scripts/mirror-repos.sh
@@ -4,7 +4,7 @@
 # You can cutomize this by providing an environment variable: "REPO_TARGET_BASE" pointing to your git repo.
 # This includes everything before the project / repo part, this means everything before "/opendevstack/..."
 #
-BASE_DIR=${REPO_MIRROR_DIR:-"/tmp"}
+BASE_DIR=${OPENDEVSTACK_DIR:-"/tmp"}
 TARGET_REPO_BASE=${REPO_TARGET_BASE:-"http://192.168.56.31:7990/scm/"}
 cwd=${PWD}
 
@@ -16,21 +16,27 @@ fi
 cd $BASE_DIR
 
 git clone --bare https://github.com/opendevstack/ods-configuration-sample.git
-git clone --bare https://github.com/opendevstack/ods-core.git
+#git clone --bare https://github.com/opendevstack/ods-core.git
 git clone --bare https://github.com/opendevstack/ods-jenkins-shared-library.git
 git clone --bare https://github.com/opendevstack/ods-provisioning-app.git
 git clone --bare https://github.com/opendevstack/ods-project-quickstarters.git
 #!/bin/sh
 cd ods-configuration-sample.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-configuration-sample.git; cd -
-cd ods-core.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-core.git; cd -
+cd ods-core.git; git pull origin; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-core.git; cd -
 cd ods-jenkins-shared-library.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-jenkins-shared-library.git; cd -
 cd ods-project-quickstarters.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-project-quickstarters.git; cd -
 cd ods-provisioning-app.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-provisioning-app.git; cd -
 #!/bin/sh
-cd ods-configuration-sample.git; git push downstream; cd -
-cd ods-core.git; git push downstream; cd -
-cd ods-jenkins-shared-library.git; git push downstream; cd -
-cd ods-project-quickstarters.git; git push downstream; cd -
-cd ods-provisioning-app.git; git push downstream; cd -
+cd ods-configuration-sample.git; git branch -d production; git push downstream; cd -
+cd ods-core.git; git branch -d production; git push downstream; cd -
+cd ods-jenkins-shared-library.git; git branch -d production; git branch production; git push downstream; cd -
+cd ods-project-quickstarters.git; git branch -d production; git branch production; git push downstream; cd -
+cd ods-provisioning-app.git; git branch -d production; git branch production; git push downstream; cd -
+
+rm -rf ods-configuration-sample.git
+rm -rf ods-core.git
+rm -rf ods-jenkins-shared-library.git
+rm -rf ods-project-quickstarters.git
+rm -rf ods-provisioning-app.git
 
 cd ${cwd}
--- infrastructure-setup/setup-local-environment.sh
+++ infrastructure-setup/setup-local-environment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Step 1/3: Setup and start VMs from Vagrant"
 vagrant up
 
 echo $PATH
