1bad3fd9b956e6282e12f4a4c2e41ec53360449f
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2018-11-22 15:24:29 +0100

    - Implemented: changed setup script

--- infrastructure-setup/setup-local-environment.sh
+++ infrastructure-setup/setup-local-environment.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Step 1/3: Setup and start VMs from Vagrant"
-vagrant up
+echo "Step 1/3: Get Configuration Sample Repository from GitHub"
+echo "Step 1/3: Get Configuration Repo from GitHub"
+echo "Step 2/3: Setup and start VMs from Vagrant"
+//vagrant up
 
 echo $PATH
