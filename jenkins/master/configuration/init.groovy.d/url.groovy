3a86e0d26c540a4ad206b4a55541ab30ea4bd0f7
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2018-11-22 16:03:33 +0100

    Set Jenkins URL during initialization
    Otherwise, no URL will be configured, which leads to an empty BUILD_URL
    until the configuration is saved in the UI.
    
    Fixes https://github.com/opendevstack/ods-jenkins-shared-library/issues/40.

---
+++ jenkins/master/configuration/init.groovy.d/url.groovy
@@ -0,0 +1,5 @@
+import jenkins.model.JenkinsLocationConfiguration
+def jlc = jenkins.model.JenkinsLocationConfiguration.get()
+def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text
+def host = "oc get route jenkins -n ${namespace} -o 'jsonpath={.spec.host}'".execute().text
+jlc.setUrl("https://${host}")
