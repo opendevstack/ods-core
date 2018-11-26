9b9ad5e4c0b7a116bfaab124f34d00d703823352
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2018-11-26 10:26:03 +0100

    Save JenkinsLocationConfiguration object

--- jenkins/master/configuration/init.groovy.d/url.groovy
+++ jenkins/master/configuration/init.groovy.d/url.groovy
@@ -3,3 +3,4 @@ def jlc = jenkins.model.JenkinsLocationConfiguration.get()
 def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text
 def host = "oc get route jenkins -n ${namespace} -o 'jsonpath={.spec.host}'".execute().text
 jlc.setUrl("https://${host}")
+jlc.save()
