42eb191421833b182d2aa5c6a0d530d8bc9592be
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2018-11-26 11:55:08 +0100

    Do not use quotes for command

--- jenkins/master/configuration/init.groovy.d/url.groovy
+++ jenkins/master/configuration/init.groovy.d/url.groovy
@@ -1,6 +1,8 @@
 import jenkins.model.JenkinsLocationConfiguration
 def jlc = jenkins.model.JenkinsLocationConfiguration.get()
-def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text
-def host = "oc get route jenkins -n ${namespace} -o 'jsonpath={.spec.host}'".execute().text
+def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text.trim()
+println "INFO: JenkinsLocationConfiguration - namespace of pod is: ${namespace}"
+def host = "oc get route jenkins -n ${namespace} -o jsonpath={.spec.host}".execute().text.trim()
+println "INFO: JenkinsLocationConfiguration - route to jenkins is: ${host}"
 jlc.setUrl("https://${host}")
 jlc.save()
