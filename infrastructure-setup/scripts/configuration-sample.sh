6b5b0d0ef08c72429075a10801c2b0d7c53f6483
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-03-07 11:23:21 +0100

    - Implemented: changed gitignore

--- infrastructure-setup/scripts/configuration-sample.sh
+++ infrastructure-setup/scripts/configuration-sample.sh
@@ -3,9 +3,12 @@
 # We assume that all ods repositories are placed next to each other
 # By default it is one level up from this script
 
-ODS_DIR=""
+ODS_DIR="../../.."
 ODS_SAMPLE_DIR=`realpath ${ODS_DIR}/ods-configuration-sample`
 ODS_CONFIG_DIR=`realpath ${ODS_DIR}/ods-configuration`
+
+echo ${ODS_SAMPLE_DIR}
+echo ${ODS_CONFIG_DIR}
 
 # check if we have a ods-configuration
 # and create one
