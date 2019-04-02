cb8b788fde28d0626463c9dae5b8c16372e915a9
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-04-02 10:47:57 +0200

    - Implemented: added http.postBuffer

--- infrastructure-setup/scripts/mirror-repos.sh
+++ infrastructure-setup/scripts/mirror-repos.sh
@@ -15,38 +15,36 @@ fi
 
 cd $BASE_DIR
 
-git config http.postBuffer 524288000
-
 echo -e "Clone repositories"
 echo -e "\nPrepare ods-configuration-sample"
 git clone --bare https://github.com/opendevstack/ods-configuration-sample.git
 cd ods-configuration-sample.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-configuration-sample.git; cd -
-cd ods-configuration-sample.git; git branch production; git push downstream; cd -
+cd ods-configuration-sample.git; git branch production; git config http.postBuffer 524288000; git push downstream; cd -
 rm -rf ods-configuration-sample.git
 
 echo -e "\nPrepare ods-core"
 git clone --bare https://github.com/opendevstack/ods-core.git
 cd ods-core.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-core.git; cd -
-cd ods-core.git; git branch production; git push downstream; cd -
+cd ods-core.git; git branch production; git config http.postBuffer 524288000; git push downstream; cd -
 rm -rf ods-core.git
 
 echo -e "\nPrepare ods-jenkins-shared-library"
 git clone --bare https://github.com/opendevstack/ods-jenkins-shared-library.git
 cd ods-jenkins-shared-library.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-jenkins-shared-library.git; cd -
-cd ods-jenkins-shared-library.git; git branch production; git push downstream; cd -
+cd ods-jenkins-shared-library.git; git branch production; git config http.postBuffer 524288000; git push downstream; cd -
 rm -rf ods-jenkins-shared-library.git
 
 
 echo -e "\nPrepare ods-provisioning-app"
 git clone --bare https://github.com/opendevstack/ods-provisioning-app.git
 cd ods-provisioning-app.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-provisioning-app.git; cd -
-cd ods-provisioning-app.git; git branch production; git push downstream; cd -
+cd ods-provisioning-app.git; git branch production; git config http.postBuffer 524288000; git push downstream; cd -
 rm -rf ods-provisioning-app.git
 
 echo -e "\nPrepare ods-project-quickstarters"
 git clone --bare https://github.com/opendevstack/ods-project-quickstarters.git
 cd ods-project-quickstarters.git; git remote add --mirror=push downstream ${TARGET_REPO_BASE}/opendevstack/ods-project-quickstarters.git; cd -
-cd ods-project-quickstarters.git; git branch production; git push downstream; cd -
+cd ods-project-quickstarters.git; git branch production; git config http.postBuffer 524288000; git push downstream; cd -
 rm -rf ods-project-quickstarters.git
 
 cd ${cwd}
