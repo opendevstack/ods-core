3c49ed745d38083c148f74e13c0caad36315f4a2
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-04-08 16:09:37 +0200

    - Added: added nexus scripts - Implemented: modified rundeck installation logic

--- infrastructure-setup/setup-local-environment.sh
+++ infrastructure-setup/setup-local-environment.sh
@@ -10,46 +10,51 @@ cd ../..
 OPENDEVSTACK_BASE_DIR=${PWD}
 
 #write base dir to local config file
-echo "Write base directory for installtion to local config file ${OPENDEVSTACK_BASE_DIR}/local.config"
-echo "OPENDEVSTACK_BASE_DIR=${PWD}" > ${OPENDEVSTACK_BASE_DIR}/local.config
 
-echo "Step 1/X: Ensure ods-core is up to date"
+if [ ! -f "${OPENDEVSTACK_BASE_DIR}/local.config" ] ; then
+  echo "Write base directory for installation to local config file ${OPENDEVSTACK_BASE_DIR}/local.config"
+  echo "OPENDEVSTACK_BASE_DIR=${PWD}" > ${OPENDEVSTACK_BASE_DIR}/local.config
+else
+  echo "Local config file exists"
+fi
+
+echo "Step 1/9: Ensure ods-core is up to date"
 cd ${cwd}/..
 git pull
 
-echo "Step 2/X: Create production branch and work from there"
+echo "Step 2/9: Create production branch and work from there"
 cd ${cwd}/..
-git checkout production
+git checkout -b production
 
-echo "Step 3/X: Clone necessary repositories and create production branch"
+echo "Step 3/9: Clone necessary repositories and create production branch"
 cd ${cwd}/scripts
 ./checkout-repositories.sh
 
-echo "Step 4/X: Configure necessary parameters for the openshift cluster environment and templates"
+echo "Step 4/9: Configure necessary parameters for the openshift cluster environment and templates"
 cd ${cwd}/scripts
 ./configure-oc-template-variables.sh
 
-echo "Step 5/X: Create configuration"
+echo "Step 5/9: Create configuration"
 cd ${cwd}/scripts
 ./create-configuration-from-sample.sh
 
-echo "Step 6/X: Replace ENV variables with preconfigured values"
+echo "Step 6/9: Replace ENV variables with preconfigured values"
 cd ${cwd}/scripts
 ./replace-oc-template-values.sh
 
-echo "Step 7/X: Init local Git repository for configuration"
+echo "Step 7/9: Init local Git repository for configuration"
 cd ${OPENDEVSTACK_BASE_DIR}
 cd ods-configuration
 git init
 git add --all
 git commit -m "Initial configuration commit"
 
-echo "Step 8/X: Setup and start VMs from Vagrant"
+echo "Step 8/9: Setup and start VMs from Vagrant"
 #start vagrant
 cd ${cwd}
 vagrant up
 
-echo "Step 9/X: Base Installation with ansible"
+echo "Step 9/9: Base Installation with ansible"
 cd ${cwd}
 vagrant ssh atlcon -c "cd /vagrant/ansible/ && export ANSIBLE_VAULT_PASSWORD_FILE=/vagrant/ansible/.vault_pass.txt && ansible-playbook -v -i inventories/dev dev.yml"
 
