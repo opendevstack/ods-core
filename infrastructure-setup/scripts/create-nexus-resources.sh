7b874cad165625ab37ec5192d716825962a0a265
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-04-08 16:18:50 +0200

    - Implemented: added nexus resources generation

--- infrastructure-setup/scripts/create-nexus-resources.sh
+++ infrastructure-setup/scripts/create-nexus-resources.sh
@@ -11,3 +11,17 @@ curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: text/plain'
 sleep 3s
 curl -u admin:admin123 --insecure -k -X DELETE "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script/createRepos"
 sleep .5s
+echo -e "Deactivate anonymous access\n"
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: application/json' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script" -d @json/deactivateAnonymous.json
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: text/plain' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script/deactivateAnonymous/run"
+sleep 3s
+curl -u admin:admin123 --insecure -k -X DELETE "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script/deactivateAnonymous"
+sleep .5s
+echo -e "Setup developer role\n"
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: application/json' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script" -d @json/createRole.json
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: text/plain' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script/createRole/run" -d @json/developer-role.json
+sleep 3s
+echo -e "Setup developer user\n"
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: application/json' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script" -d @json/createUser.json
+curl -u admin:admin123 --insecure -k -X POST --header 'Content-Type: text/plain' "https://nexus-cd.192.168.56.101.nip.io/service/rest/v1/script/createUser/run" -d @json/developer-user.json
+sleep .5s
