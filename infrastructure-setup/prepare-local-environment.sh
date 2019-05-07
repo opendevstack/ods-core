768f830c76fafbecc81765a2cf92b5e5a9e04371
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-05-07 10:51:34 +0200

    - Implemented: added steps

--- infrastructure-setup/prepare-local-environment.sh
+++ infrastructure-setup/prepare-local-environment.sh
@@ -9,39 +9,37 @@ if [ ! -d "$OPENDEVSTACK_BASE_DIR/certs" ] ; then
 fi
 
 #Activate single sign on
-echo "Step X/X: Enable Jira SSO"
+echo "Step 1/10: Enable Jira SSO"
 cd ${cwd}
 #vagrant ssh atlcon -c "cd /vagrant/ansible/ && export ANSIBLE_VAULT_PASSWORD_FILE=/vagrant/ansible/.vault_pass.txt && ansible-playbook -v -i inventories/dev playbooks/jira-enable-sso.yml"
 
-echo "Step X/X: Enable Confluence SSO"
+echo "Step 2/10: Enable Confluence SSO"
 cd ${cwd}
 #vagrant ssh atlcon -c "cd /vagrant/ansible/ && export ANSIBLE_VAULT_PASSWORD_FILE=/vagrant/ansible/.vault_pass.txt && ansible-playbook -v -i inventories/dev playbooks/confluence-enable-sso.yml"
 
-echo "Step X/X: Mirror repositories to ${TARGET_REPO_BASE}"
+echo "Step 3/10: Mirror repositories to ${TARGET_REPO_BASE}"
 cd ${cwd}/scripts
 #./mirror-repositories-to-gitserver.sh
 
-echo "Step X/X: Connect to openshift VM and prepare OpenShift cluster"
+echo "Step 4/10: Connect to openshift VM and prepare OpenShift cluster"
 vagrant ssh openshift -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-openshift-cluster.sh"
 
 sleep 5s
 
-echo "Step X/X: Add OpenShift certificate to atlassian VM"
+echo "Step 5/10: Add OpenShift certificate to atlassian VM"
 vagrant ssh atlassian -c "sudo /ods/ods-core/infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh"
 
-echo "Step X/X: Prepare Nexus"
+echo "Step 6/10: Prepare Nexus"
 vagrant ssh openshift -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-nexus.sh"
 
-echo "Step X/X: Prepare Sonarqube"
+echo "Step 7/10: Prepare Sonarqube"
 vagrant ssh openshift -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-sonarqube.sh"
 
-echo "Step X/X: Prepare Jenkins Builds"
+echo "Step 8/10: Prepare Jenkins Builds"
 vagrant ssh openshift -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-jenkins-builds.sh"
 
-echo "Step X/X: Prepare Provisioning App"
+echo "Step 9/10: Prepare Provisioning App"
 vagrant ssh openshift -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-provisioning-app.sh"
 
-echo "Step X/X: Prepare Provisioning App"
+echo "Step 10/10: Prepare Provisioning App"
 vagrant ssh atlassian -c "sudo /ods/ods-core/infrastructure-setup/scripts/prepare-rundeck.sh"
-
-
