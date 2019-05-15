3ca1c6e390fb4e7a6e859ae738666e8d6ffad5f3
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-05-15 08:26:06 +0200

    - Implemented: added service restart for bitbucket service

--- infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
+++ infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
@@ -13,3 +13,6 @@ fi
 openssl s_client -connect 192.168.56.101:8443 -showcerts < /dev/null 2>/dev/null| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${BASE_DIR}/openshift.crt
 yes yes | /etc/alternatives/jre/bin/keytool -import -alias openshift -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/openshift.crt
 yes yes | /etc/alternatives/jre/bin/keytool -import -alias oc_router -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/certs/router.crt
+
+#restart bitbucket
+systemctl restart atlbitbucket
