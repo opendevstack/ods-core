799e52e9349d8411da4d11be2ed7159b04ce5255
Torsten Jaeschke <torsten.jaeschke@opitz-consulting.com>
2019-05-15 07:40:22 +0200

    - Implemented: changed path for import

--- infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
+++ infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
@@ -11,6 +11,5 @@ if [ "$HOSTNAME" != "atlassian" ] ; then
 fi
 
 openssl s_client -connect 192.168.56.101:8443 -showcerts < /dev/null 2>/dev/null| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${BASE_DIR}/openshift.crt
-yes yes | /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/bin/keytool -import -alias openshift -keystore /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/openshift.crt
-yes yes | /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/bin/keytool -import -alias oc_router -keystore /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/certs/router.crt
-#yes yes | /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/bin/keytool -import -alias oc_registry -keystore /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/certs/registry.crt
+yes yes | /etc/alternatives/jre/bin/keytool -import -alias openshift -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/openshift.crt
+yes yes | /etc/alternatives/jre/bin/keytool -import -alias oc_router -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/certs/router.crt
