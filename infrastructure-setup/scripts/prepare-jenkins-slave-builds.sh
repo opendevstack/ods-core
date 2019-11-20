eff6f539c0d790a9fed58f43545b330d2ee72c53
Stefan Lack <stefan.lack@opitz-consulting.com>
2019-11-20 13:39:10 +0100

    added missing slave builds

--- infrastructure-setup/scripts/prepare-jenkins-slave-builds.sh
+++ infrastructure-setup/scripts/prepare-jenkins-slave-builds.sh
@@ -12,6 +12,15 @@ fi
 oc login -u system:admin
 oc project cd
 
+cd ${BASE_DIR}/ods-quickstarters/common/jenkins-slaves/airflow/ocp-config
+yes 'y' | tailor update -v --force
+
+cd ${BASE_DIR}/ods-quickstarters/common/jenkins-slaves/golang/ocp-config
+yes 'y' | tailor update -v --force
+
+cd ${BASE_DIR}/ods-quickstarters/common/jenkins-slaves/maven/ocp-config
+yes 'y' | tailor update -v --force
+
 cd ${BASE_DIR}/ods-quickstarters/common/jenkins-slaves/nodejs8-angular/ocp-config
 yes 'y' | tailor update -v --force
 
@@ -24,7 +33,10 @@ yes 'y' | tailor update -v --force
 cd ${BASE_DIR}/ods-quickstarters/common/jenkins-slaves/scala/ocp-config/
 yes 'y' | tailor update -v --force
 
-oc start-build -n cd jenkins-slave-python --follow
-oc start-build -n cd jenkins-slave-scala --follow
+oc start-build -n cd jenkins-slave-airflow --follow
+oc start-build -n cd jenkins-slave-golang --follow
+oc start-build -n cd jenkins-slave-maven --follow
 oc start-build -n cd jenkins-slave-nodejs8-angular --follow
 oc start-build -n cd jenkins-slave-nodejs10-angular --follow
+oc start-build -n cd jenkins-slave-python --follow
+oc start-build -n cd jenkins-slave-scala --follow
