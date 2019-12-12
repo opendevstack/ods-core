74196704da3459b0c89ea24f08dfe1befdc8bf42
Wruck_Schneider,Dr.,Hugo (BI X) BIX-DE-I <hugo.wruck_schneider@boehringer-ingelheim.com>
2019-12-12 09:12:22 +0100

    Testing if namespaces exists

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/opendevstack/ods-core
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20191210221141-98df12377212 // indirect
+require (
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/openshift/api v3.9.0+incompatible // indirect
+	github.com/openshift/client-go v3.9.0+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	gotest.tools v2.2.0+incompatible
+	k8s.io/api v0.17.0 // indirect
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20191114200735-6ca3b61696b6 // indirect
+)
--- tests/create-projects/create-projects_test.go
+++ tests/create-projects/create-projects_test.go
@@ -1,31 +1,77 @@
 package create_projects
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path"
-	"runtime"
-	"testing"
+    "fmt"
+    projectClientV1 "github.com/openshift/client-go/project/clientset/versioned/typed/project/v1"
+    "gotest.tools/assert"
+    meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/tools/clientcmd"
+    "os"
+    "os/exec"
+    "path"
+    "path/filepath"
+    "runtime"
+    "testing"
 )
 
+func homeDir() string {
+    if h := os.Getenv("HOME"); h != "" {
+        return h
+    }
+    return os.Getenv("USERPROFILE") // windows
+}
+
 func TestCreateProjectWithoutProjectId(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "..", "..")
-	cmd := exec.Command("sh", fmt.Sprintf("%s/create-projects/create-projects.sh", dir))
-	out, err := cmd.CombinedOutput()
-	if err == nil {
-		t.Errorf("Execution of `create-project.sh` must fail if no PROJECT_ID is set: %s", out)
-	}
+    _, filename, _, _ := runtime.Caller(0)
+    dir := path.Join(path.Dir(filename), "..", "..")
+    cmd := exec.Command("sh", fmt.Sprintf("%s/create-projects/create-projects.sh", dir))
+    out, err := cmd.CombinedOutput()
+    if err == nil {
+        t.Errorf("Execution of `create-project.sh` must fail if no PROJECT_ID is set: %s", out)
+    }
 }
 
 func TestCreateProject(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "..", "..")
-	cmd := exec.Command("sh", fmt.Sprintf("%s/create-projects/create-projects.sh", dir))
-	cmd.Env = append(os.Environ(), "PROJECT_ID=unitt")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("Execution of `create-project.sh` failed: %s\n%s", err.Error(), out)
-	}
+    _, filename, _, _ := runtime.Caller(0)
+    dir := path.Join(path.Dir(filename), "..", "..")
+    cmd := exec.Command("sh", fmt.Sprintf("%s/create-projects/create-projects.sh", dir))
+    cmd.Env = append(os.Environ(), "PROJECT_ID=unitt")
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+        t.Errorf("Execution of `create-project.sh` failed: %s\n%s", err.Error(), out)
+    }
+    config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homeDir(), ".kube", "config"))
+    if err != nil {
+        t.Errorf("Cannot load cluster configuration: %s", err.Error())
+    }
+    client, err := projectClientV1.NewForConfig(config)
+    if err != nil {
+        t.Errorf("Cannot initialize Project Client: %s", err.Error())
+    }
+    projects, err := client.Projects().List(meta_v1.ListOptions{})
+    if err != nil {
+        t.Errorf("Cannot list projects: %s", err.Error())
+    }
+
+    foundCd := false
+    foundTest := false
+    foundDev := false
+
+    for _, project := range projects.Items {
+        switch project.Name {
+        case "unitt-cd":
+            foundCd = true
+        case "unitt-test":
+            foundTest = true
+        case "unitt-dev":
+            foundDev = true
+        default:
+
+        }
+    }
+
+    assert.Equal(t, true, foundCd, "CD Project not found")
+    assert.Equal(t, true, foundDev, "Test Project not found")
+    assert.Equal(t, true, foundTest, "Dev Project not found")
+
 }
