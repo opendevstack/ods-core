360b362325dd686abf4508e83df841a1de5a6b76
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2020-01-31 14:00:58 +0100

    Read ODS configuration in boot script

--- jenkins/master/configuration/init.groovy.d/ods-mro-jenkins-shared-library.groovy
+++ jenkins/master/configuration/init.groovy.d/ods-mro-jenkins-shared-library.groovy
@@ -1,6 +1,7 @@
 #!groovy
 
 // imports
+import groovy.json.JsonSlurper
 import hudson.scm.SCM
 import jenkins.model.Jenkins
 import jenkins.plugins.git.GitSCMSource
@@ -8,33 +9,35 @@ import org.jenkinsci.plugins.workflow.libs.*
 import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
 import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
 
-def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text.trim()
-println "INFO: Jenkins adding mro shared lib into namespace: ${namespace}"
-
 def buildSharedLibName = "ods-jenkins-shared-library"
 def mroSharedLibName = "ods-mro-jenkins-shared-library"
 
+def namespace = "cat /var/run/secrets/kubernetes.io/serviceaccount/namespace".execute().text.trim()
 def env = System.getenv()
-def buildSharedLibraryRepository = env['SHARED_LIBRARY_REPOSITORY']
-
-println "INFO: Jenkins adding mro shared lib - build lib path ${buildSharedLibraryRepository}"
-
-def mroLibRepoPath = buildSharedLibraryRepository.replace(buildSharedLibName, mroSharedLibName)
-
-println "INFO: Jenkins adding mro shared lib path: ${mroLibRepoPath}, name ${mroSharedLibName}"
-
 def credentialsId = namespace + "-cd-user-with-password"
-// parameters
+def buildSharedLibRepository = env['SHARED_LIBRARY_REPOSITORY']
+// Infer repository path from SHARED_LIBRARY_REPOSITORY.
+def mroSharedLibRepository = buildSharedLibRepository.replace(buildSharedLibName, mroSharedLibName)
+
+println "INFO: Adding global library ${mroSharedLibName}: ${mroSharedLibRepository}"
+
+// Read ODS configuration.
+def jsonSlurper = new JsonSlurper()
+def odsConfig = jsonSlurper.parse(new File("/etc/opendevstack/config.json"))
+println "INFO: Read ODS configuration ${odsConfig}"
+
+// Define parameters.
+def defaultBranch = odsConfig.odsGitRef ?: 'production'
 def globalLibrariesParameters = [
-  branch:               "production",
+  branch:               defaultBranch,
   credentialId:         credentialsId,
   implicit:             false,
   name:                 mroSharedLibName,
-  repository:           mroLibRepoPath
+  repository:           mroSharedLibRepository
 ]
 
-// define global library
-GitSCMSource gitSCMSource = new GitSCMSource(
+// Define SCM retriever.
+def gitSCMSource = new GitSCMSource(
   globalLibrariesParameters.name,
   globalLibrariesParameters.repository,
   globalLibrariesParameters.credentialId,
@@ -42,38 +45,39 @@ GitSCMSource gitSCMSource = new GitSCMSource(
   "",
   false
 )
+def scmSourceRetriever = new SCMSourceRetriever(gitSCMSource)
 
-// define retriever
-SCMSourceRetriever sCMSourceRetriever = new SCMSourceRetriever(gitSCMSource)
-
-// get Jenkins instance
+// Get Jenkins instance.
 Jenkins jenkins = Jenkins.getInstance()
 
-// get Jenkins Global Libraries
+// Get global libraries.
 def globalLibraries = jenkins.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
+def List<LibraryConfiguration> existingLibs = globalLibraries.get().getLibraries()
 
-// define new library configuration
-LibraryConfiguration libraryConfiguration = new LibraryConfiguration(globalLibrariesParameters.name, sCMSourceRetriever)
+// Define new library configuration.
+def libraryConfiguration = new LibraryConfiguration(
+  globalLibrariesParameters.name,
+  scmSourceRetriever
+)
 libraryConfiguration.setDefaultVersion(globalLibrariesParameters.branch)
 libraryConfiguration.setImplicit(globalLibrariesParameters.implicit)
 
-// set new Jenkins Global Library
-def List<LibraryConfiguration> existingLibs = globalLibraries.get().getLibraries()
-
-println "INFO: Found existing libraries: " + existingLibs.size
-
-for (LibraryConfiguration config : existingLibs) {
-  if (config.getName() == mroSharedLibName) {
-    println "DEBUG: mro lib already existing, skipping"
+// Set new global library.
+def libIndex = null
+existingLibs.eachWithIndex { item, index ->
+  if (item.getName() == mroSharedLibName) {
+    libIndex = index
+    println "INFO: Library ${mroSharedLibName} exists already"
     return
   }
 }
+if (libIndex != null) {
+  println "INFO: Replacing library ${mroSharedLibName}"
+  existingLibs[libIndex] = libraryConfiguration
+} else {
+  println "INFO: Adding library ${mroSharedLibName}"
+  existingLibs.add(libraryConfiguration)
+}
 
-println "DEBUG: Adding MRO build shared lib"
-
-// add the lib
-existingLibs.add(libraryConfiguration)
-
-
-// save current Jenkins state to disk
+// Save current Jenkins state to disk.
 jenkins.save()
