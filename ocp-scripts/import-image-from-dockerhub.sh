ed86ef07107eaa43e09256c93eeb9d6153c2b27c
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-05-12 17:03:35 +0200

    address github comments

--- ocp-scripts/import-image-from-dockerhub.sh
+++ ocp-scripts/import-image-from-dockerhub.sh
@@ -21,10 +21,10 @@ echo_info(){
 }
 
 ODS_AT_DOCKERHUB=docker.io/opendevstackorg
-IMAGE_NAME=
-IMAGE_VERSION=
-NAMESPACE=ods
-TARGET_IMAGE_STREAM=
+IMAGE=
+IMAGE_TAG=
+NAMESPACE=
+TARGET_STREAM=
 
 function usage {
     printf "Import dockerhub image/tags.\n\n"
@@ -32,10 +32,10 @@ function usage {
     printf "However, you can also pass them directly. Usage:\n\n"
     printf "\t-h|--help\t\tPrint usage\n"
     printf "\t-v|--verbose\t\tEnable verbose mode\n"
-    printf "\t-on|--namespace\t\tNamespace, e.g. 'ods'\n"
-    printf "\t-in|--imagename\t\tImagename, e.g. 'ods-provisioning-app'\n"
-    printf "\t-it|--targetimagestream\t\tTargetImageName, e.g. 'ods-provisioning-app'\n"
-    printf "\t-iv|--imageversion\t\tImageversion, e.g. '2.x'\n"
+    printf "\t-n|--namespace\t\tNamespace, e.g. 'ods'\n"
+    printf "\t-i|--image\t\tImage, e.g. 'ods-provisioning-app'\n"
+    printf "\t-t|--image-tag\t\tImagetag, e.g. '2.x'\n"
+    printf "\t-s|--target-stream\t\tTarget image, e.g. 'ods-provisioning-app'\n"
 }
 
 while [[ "$#" -gt 0 ]]; do
@@ -45,38 +45,31 @@ while [[ "$#" -gt 0 ]]; do
 
     -h|--help) usage; exit 0;;
 
-    -on|--namespace) NAMESPACE="$2"; shift;;
-    -on=*|--namespace=*) NAMESPACE="${1#*=}";;
+    -n|--namespace) NAMESPACE="$2"; shift;;
+    -n=*|--namespace=*) NAMESPACE="${1#*=}";;
 
-    -in|--imagename) IMAGE_NAME="$2"; shift;;
-    -in=*|--imagename=*) IMAGE_NAME="${1#*=}";;
+    -i|--image) IMAGE="$2"; shift;;
+    -i=*|--image=*) IMAGE="${1#*=}";;
 
-    -iv|--imageversion) IMAGE_VERSION="$2"; shift;;
-    -iv=*|--imageversion=*) IMAGE_VERSION="${1#*=}";;
+    -t|--image-tag) IMAGE_TAG="$2"; shift;;
+    -t=*|--image-tag=*) IMAGE_TAG="${1#*=}";;
 
-    -it|--targetimagestream) TARGET_IMAGE_STREAM="$2"; shift;;
-    -it=*|--targetimagestream=*) TARGET_IMAGE_STREAM="${1#*=}";;
+    -s|--target-stream) TARGET_STREAM="$2"; shift;;
+    -s=*|--target-stream=*) TARGET_STREAM="${1#*=}";;
 
     *) echo_error "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-if ! which jq >/dev/null; then
-    echo_warn "Binary 'jq' (https://stedolan.github.io/jq/) is not in your PATH. This will make the script less comfortable to use."
-    read -r -e -p "Continue anyway? [y/n] " input
-    if [ "${input:-""}" != "y" ]; then
-        exit 1
-    fi
-fi
-
 if [ -z "${NAMESPACE}" ]; then
-    odsprojectname="odsst-dev"
+    odsprojectname=
     if [ -f "${ODS_CORE_DIR}/../ods-configuration/ods-core.env" ]; then
         echo_info "Configuration located"
-        odsprojectname=$(grep NAMESPACE "${ODS_CORE_DIR}/../ods-configuration/ods-core.env" | cut -d "=" -f 2-)
+        odsprojectname=$(grep ODS_NAMESPACE "${ODS_CORE_DIR}/../ods-configuration/ods-core.env" | cut -d "=" -f 2-)
     fi
     read -r -e -p "Enter ods central namespace [${odsprojectname}]: " input
     if [[ -z "${input}" || $(echo -n $input | wc -m) == 0 ]]; then
-        NAMESPACE=${odsprojectname}
+    	NAMESPACE=$odsprojectname
+        echo_info "Setting namespace to ${odsprojectname}"
     else
         NAMESPACE=${input:-""}
     fi
@@ -87,56 +80,58 @@ if ! oc project ${NAMESPACE} -q; then
     exit 1
 fi
 
-if [ -z "${IMAGE_NAME}" ]; then
+if [ -z "${IMAGE}" ]; then
     read -r -e -p "Enter image name: " input
-    if [ -z "${input}" ]; then
-        echo_error "Image name is mandatory, aborting without it"
+    if [[ -z "${input}" || $(echo -n $input | wc -m) == 0 ]]; then
+        echo_error "Image name 'image' is mandatory, aborting without it"
         exit 1
     else
-        IMAGE_NAME=${input:-""}
+        IMAGE=${input:-""}
     fi
 fi
 
-if [ -z "${TARGET_IMAGE_STREAM}" ]; then
-    read -r -e -p "Enter target image stream name: " input
-    if [ -z "${input}" ]; then
-        TARGET_IMAGE_STREAM=$IMAGE_NAME
-        echo "Target not set, defaulting to source: $IMAGE_NAME"
+if [ -z "${TARGET_STREAM}" ]; then
+    read -r -e -p "Enter target image stream name [${IMAGE}]: " input
+    if [[ -z "${input}" || $(echo -n $input | wc -m) == 0 ]]; then
+        TARGET_STREAM=$IMAGE
+        echo_info "Target image not set, defaulting to source: $IMAGE"
     else
-        TARGET_IMAGE_STREAM=${input:-""}
+        TARGET_STREAM=${input:-""}
     fi
 fi
 
-if ! oc -n ${NAMESPACE} get is ${TARGET_IMAGE_STREAM} >/dev/null; then
-	echo_error "Target image stream ${TARGET_IMAGE_STREAM} does not exist in project ${NAMESPACE}. Did you run 'tailor apply'?"
+if ! oc -n ${NAMESPACE} get is ${TARGET_STREAM} >/dev/null; then
+	echo_error "Target image stream ${TARGET_STREAM} does not exist in project ${NAMESPACE}. Did you run 'tailor apply'?"
 	exit 1
 fi
 
-if [ -z "${IMAGE_VERSION}" ]; then
-    imageversion="latest"
+if [ -z "${IMAGE_TAG}" ]; then
+    imageversion=
     if [ -f "${ODS_CORE_DIR}/../ods-configuration/ods-core.env" ]; then
         echo_info "Configuration located"
         imageversion=$(grep ODS_IMAGE_TAG "${ODS_CORE_DIR}/../ods-configuration/ods-core.env" | cut -d "=" -f 2-)
     fi
     read -r -e -p "Enter image version [${imageversion}]: " input
-    if [ -z "${input}" ]; then
-        IMAGE_VERSION=${imageversion}
+    if [[ -z "${input}" || $(echo -n $input | wc -m) == 0 ]]; then
+        IMAGE_TAG=${imageversion}
+        echo_info "Imagetag not set, setting to: $imageversion"
     else
-        IMAGE_VERSION=${input:-""}
+        IMAGE_TAG=${input:-""}
     fi
 fi
 
-TAG_SOURCE=${ODS_AT_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_VERSION}
-TAG_TARGET=${NAMESPACE}/${TARGET_IMAGE_STREAM}:${IMAGE_VERSION}
+TAG_SOURCE=${ODS_AT_DOCKERHUB}/${IMAGE}:${IMAGE_TAG}
+TAG_TARGET=${NAMESPACE}/${TARGET_STREAM}:${IMAGE_TAG}
 
 echo_info "importing remote image ${TAG_SOURCE} into local ${TAG_TARGET}"
 
-# tag directly - this should fail after a couple seconds, so we can verify
+# this is an async call, so we wait 5 secs and then get the tag, which will fail
+# in case errors happened
 oc tag ${TAG_SOURCE} ${TAG_TARGET} --scheduled
 
 sleep 5
 
-if ! oc -n ${NAMESPACE} get istag ${TARGET_IMAGE_STREAM}:${IMAGE_VERSION} &> /dev/null; then
+if ! oc -n ${NAMESPACE} get istag ${TARGET_STREAM}:${IMAGE_TAG} &> /dev/null; then
 	echo_error "Could not import tag - please check image stream logs '${TAG_TARGET}'"
 	exit 1
 fi
