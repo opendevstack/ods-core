254bd430fe47fbd65d32d5190e155f88cf3495a6
Jan Frank <jan.frank@boehringer-ingelheim.com>
2020-05-18 16:14:55 +0200

    change service-ca import to runtime for sonarcube and jenkins

--- sonarqube/run.sh
+++ sonarqube/run.sh
@@ -22,6 +22,17 @@ for FILENAME in /opt/configuration/sonarqube/plugins/*; do
   cp "${FILENAME}" "${SONARQUBE_HOME}/extensions/plugins/${plugin}"
 done
 
+# Openshift default CA. See https://docs.openshift.com/container-platform/3.11/dev_guide/secrets.html#service-serving-certificate-secrets
+SERVICEACCOUNT_CA='/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
+if [[ -f $SERVICEACCOUNT_CA ]]; then
+  echo "INFO: found $SERVICEACCOUNT_CA"
+  echo "INFO: importing into cacerts"
+  keytool -importcert -v -trustcacerts -alias service-ca -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -file $SERVICEACCOUNT_CA -noprompt
+else
+  echo "INFO: could not find '$SERVICEACCOUNT_CA'"
+  echo "INFO: skip import"
+fi
+
 exec java -jar "lib/sonar-application-${SONAR_VERSION}.jar" \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="${SONARQUBE_JDBC_USERNAME}" \
