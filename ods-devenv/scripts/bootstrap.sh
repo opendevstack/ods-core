afe78cffb15dacf20c1b5af4093cf20985d9da50
Georg Federmann <georg.federmann@gmail.com>
2020-05-18 09:36:13 +0200

    Revert "fix Jenkins build"
    This reverts commit 08f945c2f85f6306bc61725f56ab1380c9806963.

--- ods-devenv/scripts/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo yum install -y git
-mkdir projects && cd projects
-git clone https://github.com/opendevstack/ods-core.git
-cd ods-core && git checkout -t origin/feature/ods-devenv
-time bash ods-devenv/scripts/deploy.sh basic_vm_setup
