775e90bce8302213099f349a678fe9bd3ba85096
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2020-05-29 14:32:46 +0200

    Make script interactive

--- tests/scripts/recreate-test-infrastructure.sh
+++ tests/scripts/recreate-test-infrastructure.sh
@@ -3,8 +3,12 @@ set -uxe
 
 ODS_CORE_DIR="${BASH_SOURCE%/*}/../.."
 
-URL=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}')
+if ! oc whoami; then
+    echo "You must be logged in to the OC Cluster"
+fi
 
+# Safeguard - only ever run against local OpenShift clusters!
+URL=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}')
 if [ ${URL} != "https://172.17.0.1:8443" ]; then
     echo "You are not in a local cluster. Stopping now!!!"
     exit 1
@@ -14,31 +18,32 @@ if ! oc get clusterroles | grep request_role; then
   oc create -f ${BASH_SOURCE%/*}/json/create-cluster-role.json
 fi
 
+# Init configuration
 if [ ! -d ${ODS_CORE_DIR}/../ods-configuration ]; then
     mkdir -p ${ODS_CORE_DIR}/../ods-configuration
 else
     rm -rf ${ODS_CORE_DIR}/../ods-configuration
     mkdir -p ${ODS_CORE_DIR}/../ods-configuration
 fi
-
-${BASH_SOURCE%/*}/create-env-from-local-cluster.sh --base-oc-dir=${HOME}/openshift.local.clusterup --output ${ODS_CORE_DIR}/../ods-configuration/ods-core.env
+${BASH_SOURCE%/*}/create-env-from-local-cluster.sh \
+    --base-oc-dir=${HOME}/openshift.local.clusterup \
+    --output ${ODS_CORE_DIR}/../ods-configuration/ods-core.env
 
 NAMESPACE="ods"
 REF="cicdtests"
-
-if ! oc whoami; then
-    echo "You must be logged in to the OC Cluster"
-fi
 
 if oc project ${NAMESPACE}; then
     oc delete project ${NAMESPACE}
 fi
 
+# Mock Bitbucket
 ${BASH_SOURCE%/*}/deploy-mocks.sh  --verbose --wait
 ${BASH_SOURCE%/*}/setup-mocked-ods-repo.sh --ods-ref ${REF} --verbose
 
+# Central namespace
 ${ODS_CORE_DIR}/ods-setup/setup-ods-project.sh --verbose --non-interactive
 
+# Jenkins resources
 cd ${ODS_CORE_DIR}/jenkins/ocp-config/build && tailor apply --namespace ${NAMESPACE} --non-interactive && cd -
 ${ODS_CORE_DIR}/ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-master
 ${ODS_CORE_DIR}/ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-slave-base
@@ -64,6 +69,7 @@ ${ODS_CORE_DIR}/create-projects/create-projects.sh --verbose \
     --project ${PROJECT_ID}
 
 ${ODS_CORE_DIR}/create-projects/create-cd-jenkins.sh --verbose \
+    --non-interactive \
     --ods-namespace ${NAMESPACE} \
     --cd-user-type general \
     --cd-user-id-b64 ${CD_USER_ID_B64} \
