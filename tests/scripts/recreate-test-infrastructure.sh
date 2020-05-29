2671404260a6db8523b91f581b84a12a6cc5dc6e
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2020-05-29 12:48:13 +0200

    Cleanup

--- tests/scripts/recreate-test-infrastructure.sh
+++ tests/scripts/recreate-test-infrastructure.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -uxe
 
+ODS_CORE_DIR="${BASH_SOURCE%/*}/../.."
 
 URL=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}')
 
@@ -13,14 +14,14 @@ if ! oc get clusterroles | grep request_role; then
   oc create -f ${BASH_SOURCE%/*}/json/create-cluster-role.json
 fi
 
-if [ ! -d ${BASH_SOURCE%/*}/../../../ods-configuration ]; then
-    mkdir -p ${BASH_SOURCE%/*}/../../../ods-configuration
+if [ ! -d ${ODS_CORE_DIR}/../ods-configuration ]; then
+    mkdir -p ${ODS_CORE_DIR}/../ods-configuration
 else
-    rm -rf ${BASH_SOURCE%/*}/../../../ods-configuration
-    mkdir -p ${BASH_SOURCE%/*}/../../../ods-configuration
+    rm -rf ${ODS_CORE_DIR}/../ods-configuration
+    mkdir -p ${ODS_CORE_DIR}/../ods-configuration
 fi
 
-${BASH_SOURCE%/*}/create-env-from-local-cluster.sh --base-oc-dir=${HOME}/openshift.local.clusterup --output ${BASH_SOURCE%/*}/../../../ods-configuration/ods-core.env
+${BASH_SOURCE%/*}/create-env-from-local-cluster.sh --base-oc-dir=${HOME}/openshift.local.clusterup --output ${ODS_CORE_DIR}/../ods-configuration/ods-core.env
 
 NAMESPACE="ods"
 REF="cicdtests"
@@ -36,16 +37,17 @@ fi
 ${BASH_SOURCE%/*}/deploy-mocks.sh  --verbose --wait
 ${BASH_SOURCE%/*}/setup-mocked-ods-repo.sh --ods-ref ${REF} --verbose
 
-${BASH_SOURCE%/*}/../../ods-setup/setup-ods-project.sh --verbose --non-interactive
+${ODS_CORE_DIR}/ods-setup/setup-ods-project.sh --verbose --non-interactive
 
-cd ${BASH_SOURCE%/*}/../../jenkins/ocp-config/build && tailor apply --namespace ${NAMESPACE} --non-interactive && cd -
-${BASH_SOURCE%/*}/../../ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-master
-${BASH_SOURCE%/*}/../../ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-slave-base
-${BASH_SOURCE%/*}/../../ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-webhook-proxy
+cd ${ODS_CORE_DIR}/jenkins/ocp-config/build && tailor apply --namespace ${NAMESPACE} --non-interactive && cd -
+${ODS_CORE_DIR}/ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-master
+${ODS_CORE_DIR}/ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-slave-base
+${ODS_CORE_DIR}/ocp-scripts/start-and-follow-build.sh --namespace ${NAMESPACE} --build-config jenkins-webhook-proxy
 
+# Read params as env variables
+source ${ODS_CORE_DIR}/../ods-configuration/ods-core.env
 
-
-source ${BASH_SOURCE%/*}/../../../ods-configuration/ods-core.env
+# Delete projects if they exist already
 PROJECT_ID=prov
 if oc project "${PROJECT_ID}-test" > /dev/null; then
     oc delete project "${PROJECT_ID}-test"
@@ -57,15 +59,11 @@ if oc project "${PROJECT_ID}-cd" > /dev/null; then
     oc delete project "${PROJECT_ID}-cd"
 fi
 
-PROJECT_ID=${PROJECT_ID} \
-    ${BASH_SOURCE%/*}/../../create-projects/create-projects.sh  --verbose -p ${PROJECT_ID}
+# Create projects
+${ODS_CORE_DIR}/create-projects/create-projects.sh  --verbose -p ${PROJECT_ID}
 
-PROJECT_ID=${PROJECT_ID}
 CD_USER_TYPE=general
 CD_USER_ID_B64=${CD_USER_ID_B64}
 PIPELINE_TRIGGER_SECRET=${PIPELINE_TRIGGER_SECRET_B64}
 
-${BASH_SOURCE%/*}/../../create-projects/create-cd-jenkins.sh --ods-namespace ${NAMESPACE} --verbose --cd-user-type ${CD_USER_TYPE} --cd-user-id-b64 ${CD_USER_ID_B64} --pipeline-trigger-secret-b64 ${PIPELINE_TRIGGER_SECRET_B64} --project ${PROJECT_ID}
-
-
-oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:prov-cd:jenkins
+${ODS_CORE_DIR}/create-projects/create-cd-jenkins.sh --ods-namespace ${NAMESPACE} --verbose --cd-user-type ${CD_USER_TYPE} --cd-user-id-b64 ${CD_USER_ID_B64} --pipeline-trigger-secret-b64 ${PIPELINE_TRIGGER_SECRET_B64} --project ${PROJECT_ID}
