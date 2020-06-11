2bc1186e31bf485116a402835e382510d46f222b
Georg Federmann <georg.federmann@gmail.com>
2020-06-11 10:56:53 +0200

    fixes

--- ods-devenv/bitbucket-docker/import_certs.sh
+++ ods-devenv/bitbucket-docker/import_certs.sh
@@ -3,7 +3,7 @@ set -eu
 
 if [[ ! -z ${APP_DNS:=""} ]]; then
     echo "Setting up certificates from APP_DNS=${APP_DNS} ..."; \
-
+    mkdir -p "/etc/pki/ca-trust/source/anchors"
     KEYSTORE="$JAVA_HOME/lib/security/cacerts"
 
     arrIN=(${APP_DNS//;/ })
@@ -18,7 +18,7 @@ if [[ ! -z ${APP_DNS:=""} ]]; then
         openssl s_client -showcerts -host ${DNS} -port ${PORT} </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "${cert_bundle_path}"
         $JAVA_HOME/bin/keytool -import -noprompt -trustcacerts -file ${cert_bundle_path} -alias ${DNS} -keystore ${KEYSTORE} -storepass changeit
     done
-    update-ca-trust
+    update-ca-certificates
     echo "Done with certificate setup"
 else
     echo 'No certificates to import'
