46a8aa53a0d16de53b86c70d57795a477234c221
Georg Federmann <georg.federmann@gmail.com>
2020-06-16 11:04:00 +0200

    parameterize git ref to build ods box against

--- ods-devenv/scripts/bootstrap.sh
+++ ods-devenv/scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo yum install -y git
-mkdir projects && cd projects
+mkdir projects && cd projects || return
 git clone https://github.com/opendevstack/ods-core.git
 cd ods-core && git checkout -t origin/feature/ods-devenv
-time bash ods-devenv/scripts/deploy.sh basic_vm_setup
+time bash ods-devenv/scripts/deploy.sh feature/ods-devenv basic_vm_setup
