e09c7f6aab4ca6d7f6ffac666ec9969da60dd34b
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-06-30 10:37:41 +0200

    add jenkins noproxy

---
+++ jenkins/master/configuration/init.groovy.d/proxy.groovy
@@ -0,0 +1,28 @@
+import jenkins.model.Jenkins
+
+Jenkins jenkins = Jenkins.getInstance()
+println("CurrentProxy: ${jenkins.proxy}")
+
+String httpProxy = System.getenv('HTTP_PROXY')
+httpProxy = httpProxy.minus(~/^https?:\/\//)
+println (httpProxy)
+String httpNOProxyHosts = System.getenv('NO_PROXY')
+String [] httpProxySplit = httpProxy.split(':')
+
+String noProxyAmended = httpNOProxyHosts.replace(',','\r')
+
+ProxyConfiguration proxy
+
+if (httpProxySplit.size() == 2) {
+    proxy = new ProxyConfiguration(
+        httpProxySplit[0], httpProxySplit[1] as int, null, null, noProxyAmended) 
+} else {
+    proxy = new ProxyConfiguration(
+        httpProxySplit[0], 80, null, null, noProxyAmended) 
+}   
+
+jenkins.proxy = proxy
+
+println("Proxy set to: ${jenkins.proxy.name}:${jenkins.proxy.port}")
+
+jenkins.save()
