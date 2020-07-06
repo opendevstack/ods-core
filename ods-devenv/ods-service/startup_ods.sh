256ff5374d056c8fcc3884af80012e33007fc379
Georg Federmann <georg.federmann@gmail.com>
2020-07-06 13:56:10 +0200

    fix ods service

--- ods-devenv/ods-service/startup_ods.sh
+++ ods-devenv/ods-service/startup_ods.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-echo openshift | sudo -S ls
+
+# make wheel pwd free
+echo openshift | sudo -S sed -i '0,/%wheel[[:space:]]*ALL=(ALL)[[:space:]]*ALL/{s||%wheel        ALL=(ALL)       NOPASSWD: ALL|}' /etc/sudoers
+
 /home/openshift/opendevstack/ods-core/ods-devenv/scripts/deploy.sh --target startup_ods
+
+# give wheel their pwd back
+echo openshift | sudo -S sed -i '0,/%wheel[[:space:]]*ALL=(ALL)[[:space:]]*NOPASSWD:[[:space:]]*ALL/{s||%wheel  ALL=(ALL)       ALL|}' /etc/sudoers
