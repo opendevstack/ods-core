692f45f6eee8fcf5035de6242983c0a26eded19a
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-07-06 10:30:02 +0200

    fix sed

--- tests/scripts/utils/print-jenkins-json-status.sh
+++ tests/scripts/utils/print-jenkins-json-status.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-JENKINS_BUILD_JSON_RAW=$(oc get build $1 -n $2 -ojson | jq '.metadata.annotations."openshift.io/jenkins-status-json"' | sed -e 's|\\||g')
+JENKINS_BUILD_JSON_RAW=$(oc get build $1 -n $2 -ojson | jq '.metadata.annotations."openshift.io/jenkins-status-json"' | sed 's|\\||g')
 echo "${JENKINS_BUILD_JSON_RAW:1:${#JENKINS_BUILD_JSON_RAW}-2}" | sed "s/=\"\([^\"]*\)\"/='\1'/g" | jq '[.stages[] | {stage: .name, status: .status}]'
