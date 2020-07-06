51f66cdb639f8580a5bddc006e6326c686433bac
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-07-06 10:24:52 +0200

    debug!

--- tests/create-projects/jenkinsfile_test.go
+++ tests/create-projects/jenkinsfile_test.go
@@ -135,6 +135,7 @@ func TestCreateProjectThruWebhookProxyJenkinsFile(t *testing.T) {
 		"tests/scripts/utils/print-jenkins-json-status.sh",
 		[]string{
 			buildName,
+			values["ODS_NAMESPACE"]
 		}, []string{})
 
 	if err != nil {
