ba3a72038fd781b3592c0809d9997a7c4cefd82e
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-07-09 17:32:52 +0200

    pkg fix

--- tests/ods-verify/verify-ods-project-provision-api_test.go
+++ tests/ods-verify/verify-ods-project-provision-api_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestVerifyOdsProjectProvisionThruProvisionApi(t *testing.T) {
 	// get (executed) jenkins stages from run - the caller can compare against the golden record 
-	stdout, _, err := RunScriptFromBaseDir(
+	stdout, stderr, err := utils.RunScriptFromBaseDir(
 		"tests/scripts/create-project-api.sh",
 		[]string{}, []string{})
 
@@ -36,9 +36,9 @@ func TestVerifyOdsProjectProvisionThruProvisionApi(t *testing.T) {
 		t.Fatal(err)
 	}
 	
-	if log != string(expected) {
-		t.Fatalf("Actual jenkins stages from prov run: %s don't match -golden:\n'%s'\n-jenkins response:\n'%s'",
-			componentId, expectedAsString, stages)
+	if string(log) != string(expected) {
+		t.Fatalf("prov run - records don't match -golden:\n'%s'\n-jenkins response:\n'%s'",
+			string(expected), log)
 	}
 
 }
