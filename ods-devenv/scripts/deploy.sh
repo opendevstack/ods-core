dd1208b842732f8468d712df0c469e47e2f511c9
Georg Federmann <georg.federmann@gmail.com>
2020-07-09 18:52:42 +0200

    add prov app auth config to env

--- ods-devenv/scripts/deploy.sh
+++ ods-devenv/scripts/deploy.sh
@@ -1312,6 +1312,10 @@ function create_configuration() {
     sed -i "s|CROWD_URL=.*$|CROWD_URL=http://${atlassian_crowd_host}:${atlassian_crowd_port_internal}/crowd|" ods-core.env
     sed -i "s/PROV_APP_CONFLUENCE_ADAPTER_ENABLED=.*$/PROV_APP_CONFLUENCE_ADAPTER_ENABLED=false/" ods-core.env
 
+    sed -i "s|PROV_APP_CONFLUENCE_ADAPTER_ENABLED=.*$|PROV_APP_CONFLUENCE_ADAPTER_ENABLED=false|" ods-core.env
+    sed -i "s|PROV_APP_AUTH_BASIC_AUTH_ENABLED=.*$|PROV_APP_AUTH_BASIC_AUTH_ENABLED=true|" ods-core.env
+    sed -i "s|PROV_APP_PROVISION_CLEANUP_INCOMPLETE_PROJECTS_ENABLED=.*$|v=false|" ods-core.env
+
     # OpenShift
     sed -i "s|OPENSHIFT_CONSOLE_HOST=.*$|OPENSHIFT_CONSOLE_HOST=https://ocp.${odsbox_domain}:8443|" ods-core.env
     sed -i "s|OPENSHIFT_APPS_BASEDOMAIN=.*$|OPENSHIFT_APPS_BASEDOMAIN=.ocp.${odsbox_domain}|" ods-core.env
