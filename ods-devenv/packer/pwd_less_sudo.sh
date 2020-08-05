ba44342c9e666beae45ebe8f1d8da56472dc8e99
Georg <georgfedermann@users.noreply.github.com>
2020-08-05 22:45:18 +0200

    Make sudoers immutable during box setup (#749)

--- ods-devenv/packer/pwd_less_sudo.sh
+++ ods-devenv/packer/pwd_less_sudo.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # make wheel pwd free
 sudo sed -i '0,/%wheel[[:space:]]*ALL=(ALL)[[:space:]]*ALL/{s||%wheel        ALL=(ALL)       NOPASSWD: ALL|}' /etc/sudoers
+sudo chattr +i /etc/sudoers
+sudo cat /etc/sudoers
--- ods-devenv/packer/pwd_sudo.sh
+++ ods-devenv/packer/pwd_sudo.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # give wheel their pwd back
+sudo chattr -i /etc/sudoers
 sudo sed -i '0,/%wheel[[:space:]]*ALL=(ALL)[[:space:]]*NOPASSWD:[[:space:]]*ALL/{s||%wheel  ALL=(ALL)       ALL|}' /etc/sudoers
