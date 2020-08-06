4e830ec90e3bdccc5fbaa4e641288846b5d7a18c
Georg <georgfedermann@users.noreply.github.com>
2020-08-06 13:25:12 +0200

    Fix sudo config and oc login (#752)

--- ods-devenv/scripts/deploy.sh
+++ ods-devenv/scripts/deploy.sh
@@ -99,7 +99,9 @@ function check_system_setup() {
 } >> ~/.bashrc
 
     # suppress sudo timeout
+    sudo chattr -i /etc/sudoers
     sudo sed -i "\$aDefaults    env_reset,timestamp_timeout=-1" /etc/sudoers
+    sudo chattr +i /etc/sudoers
 
     # remove obsolete version of git
     if [[ -n $(command -v git) ]]
@@ -397,6 +399,7 @@ function setup_openshift_cluster() {
     oc create route edge --service=docker-registry --hostname="docker-registry-default.ocp.odsbox.lan" -n default
 
     oc adm policy add-cluster-role-to-user cluster-admin developer
+    oc login -u developer -p anypwd
     # allow for OpenShifts to be resolved within OpenShift network
     sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 
@@ -1518,6 +1521,9 @@ function setup_docgen() {
 #   None
 #######################################
 function setup_jenkins_agents() {
+    # to be save login as developer again
+    oc login -u developer -p anypwd
+
     local opendevstack_dir="${HOME}/opendevstack"
     local quickstarters_jenkins_agents_dir="${opendevstack_dir}/ods-quickstarters/common/jenkins-agents"
     local ocp_config_folder="ocp-config"
@@ -1535,6 +1541,7 @@ function setup_jenkins_agents() {
     for technology in $(ls -d -- */)
     do
         technology=${technology%/*}
+        echo "Current user $(oc whoami)"
         pushd "${technology}/${ocp_config_folder}"
         echo "Creating build configuration of jenkins-agent for technology ${technology}."
         tailor apply --verbose --force --non-interactive &
