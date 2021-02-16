33e9d5b4d6c7efd82e0907aa0b92508f26d51acb
Titakis,Sebastian (BI X) BIX-DE-I <sebastian.titakis@boehringer-ingelheim.com>
2021-02-16 12:40:51 +0100

    adds pub key parameter to buildbot

--- ods-devenv/buildbot/scripts/runAmiBuild.sh
+++ ods-devenv/buildbot/scripts/runAmiBuild.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # Per default this script will build a new AMI on AWS for the ODS master branch.
 # The branch can be overridden to build e.g. 3.x. Caveat: the stated branch must
-# exist on each of the following repositories: 
+# exist on each of the following repositories:
 # ods-core, ods-quickstarters, ods-jenkins-shared-library, not necessarily in ods-document-generation-templates
 set -exu
 
+pub_key=
 targetGitRef="master"
 readonly repository=ods-core
 readonly odsCoreClonePath=https://github.com/opendevstack/ods-core
@@ -18,6 +19,7 @@ while [[ "$#" -gt 0 ]]; do
   --instance_type) instanceType="$2"; shift;;
   --aws_access_key) awsAccessKey="$2"; shift;;
   --aws_secret_access_key) awsSecretAccessKey="$2"; shift;;
+  --pub_key) pub_key="$2"; shift;;
 
 esac; shift; done
 
@@ -53,7 +55,7 @@ rm -f "${logPath}/current_${targetGitRefForPath}.log"
 ln -s "${logFile}" "${logPath}/current_${targetGitRefForPath}.log"
 
 # run packer build
-time bash 2>&1 ods-devenv/packer/create_ods_box_image.sh --target create_ods_box_ami --aws-access-key "${awsAccessKey:?}" --aws-secret-key "${awsSecretAccessKey:?}" --ods-branch "${targetGitRef}" --instance-type "${instanceType:?}" | tee "${logFile}"
+time bash 2>&1 ods-devenv/packer/create_ods_box_image.sh --target create_ods_box_ami --aws-access-key "${awsAccessKey:?}" --aws-secret-key "${awsSecretAccessKey:?}" --ods-branch "${targetGitRef}" --pub-key "${pub_key}" --instance-type "${instanceType:?}"  | tee "${logFile}"
 
 # clean up after build
 cd ../..
