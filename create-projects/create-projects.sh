47ac4cd51db8083a0481ebaef130414c1f04b1e6
zxBCN Cruz_Martinez <felipe.cruz_martinez.ext@boehringer-ingelheim.com>
2021-03-03 08:47:12 +0100

    Updated label to opendevstack.org/project.owner

--- create-projects/create-projects.sh
+++ create-projects/create-projects.sh
@@ -85,12 +85,12 @@ if [ -n "${PROJECT_ADMINS}" ]; then
   # Take the first in the list as owner of the project.
   # Labels do not allow the '@' char so let's replace it with '_at_'
   # For instance, foo@bar.com will be converted to foo_at_bar.com
-  # resulting in the following label: ods.project.owner=foot_at_bar.com
+  # resulting in the following label: opendevstack.org/project.owner=foot_at_bar.com
   namespace_owner=$(echo "${PROJECT_ADMINS}" | cut -d "," -f1 | sed -r 's/@/_at_/g')
 
-  oc label namespace "${PROJECT_ID}-dev" ods.project.owner="${namespace_owner}"
-  oc label namespace "${PROJECT_ID}-test" ods.project.owner="${namespace_owner}"
-  oc label namespace "${PROJECT_ID}-cd" ods.project.owner="${namespace_owner}"
+  oc label namespace "${PROJECT_ID}-dev" opendevstack.org/project.owner="${namespace_owner}"
+  oc label namespace "${PROJECT_ID}-test" opendevstack.org/project.owner="${namespace_owner}"
+  oc label namespace "${PROJECT_ID}-cd" opendevstack.org/project.owner="${namespace_owner}"
 fi
 
 if [ -n "${PROJECT_GROUPS}" ]; then
