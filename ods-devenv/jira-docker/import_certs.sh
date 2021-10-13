ea2f49df0acb19ded21cceb96446c923c49a23df
Clemens Utschig <40628552+clemensutschig@users.noreply.github.com>
2021-10-13 18:01:57 +0200

    Update import-certs with the new java cacert location
    copy fix from jenkins-base

--- ods-devenv/jira-docker/import_certs.sh
+++ ods-devenv/jira-docker/import_certs.sh
@@ -4,7 +4,7 @@ set -eu
 if [[ ! -z ${APP_DNS:=""} ]]; then
     echo "Setting up certificates from APP_DNS=${APP_DNS} ..."; \
     mkdir -p "/etc/pki/ca-trust/source/anchors"
-    KEYSTORE="$JAVA_HOME/jre/lib/security/cacerts"
+    KEYSTORE="$JAVA_HOME/lib/security/cacerts"
 
     arrIN=(${APP_DNS//;/ })
     for val in "${arrIN[@]}";
