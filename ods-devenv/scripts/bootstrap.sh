8642938287d93914e7c18618d11b0aaab28c6ff4
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-06-09 16:33:39 +0200

    xrdp needs to be installed from sources.

--- ods-devenv/scripts/bootstrap.sh
+++ ods-devenv/scripts/bootstrap.sh
@@ -27,7 +27,7 @@ echo " "
 sudo yum update -y || true
 sudo yum install -y yum-utils epel-release https://repo.ius.io/ius-release-el7.rpm || true
 sudo yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm || true
-sudo yum -y install git gitk iproute lsof xrdp tigervnc-server remmina firewalld git2u-all glances golang jq tree \
+sudo yum -y install git gitk iproute lsof tigervnc-server remmina firewalld git2u-all glances golang jq tree \
             etckeeper unzip \
             adoptopenjdk-8-hotspot adoptopenjdk-11-hotspot adoptopenjdk-8-hotspot-jre adoptopenjdk-11-hotspot-jre \
             || true
