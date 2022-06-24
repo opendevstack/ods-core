8a945ea44bbab9cce196d0405c1010e83ac23e22
zxBCN Pablos_Ceruelo,Victor (IT EDS) EXTERNAL <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-06-24 10:18:11 +0000

    Implemented build quickstaters order.

---
+++ tests/utils/sort-testable-quickstarters.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "testing"
+    "strings"
+)
+
+func SortTestableQuickstarters(t *testing.T, dir string, quickstarterPaths []string) []string {
+
+    var filePath string
+    filePath = fmt.Sprintf("%s/../ods-core/tests/%s", dir, "quickStartersBuildPriority.txt")
+    fmt.Printf("\n\nLooking for file quickStartersBuildPriority.txt ... %s\n", filePath)
+
+	fmt.Printf("\n\nNot sorted quickStarters list:\n")
+	for _, quickstarterPath := range quickstarterPaths {
+		fmt.Printf("- %s\n", quickstarterPath)
+	}
+	fmt.Printf("\n\n")
+
+    var sortedQuickStartersPaths []string
+
+    file, err := os.Open(filePath)
+    if err != nil {
+        fmt.Println(err)
+        t.Fatal(err)
+    }
+    defer file.Close()
+
+    fmt.Println("\n\nQuickStarters that will be compiled first...\n")
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        var qsSubText = scanner.Text()
+        fmt.Println(qsSubText)
+        for _, quickstarterPath := range quickstarterPaths {
+            if strings.Contains(quickstarterPath, qsSubText) {
+                sortedQuickStartersPaths = append(sortedQuickStartersPaths, quickstarterPath)
+            }
+        }
+
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println(err)
+        t.Fatal(err)
+    }
+
+    for _, quickStarterPath := range quickstarterPaths {
+        var addQs = true
+        for _, sortedQuickStarterPath := range sortedQuickStartersPaths {
+            if strings.Contains(quickStarterPath, sortedQuickStarterPath) {
+                addQs = false
+            }
+        }
+
+        if addQs {
+            sortedQuickStartersPaths = append(sortedQuickStartersPaths, quickStarterPath)
+        }
+    }
+
+    return sortedQuickStartersPaths
+}
