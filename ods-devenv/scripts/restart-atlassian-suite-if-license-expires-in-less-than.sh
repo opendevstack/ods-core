cb2311eb0200c241ee9e1295474bcdc344356696
zxBCN Pablos_Ceruelo,Victor (IT EDS) EXTERNAL <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-08-26 08:53:52 +0000

    Script that restarts atlassian stack if we need to.

--- ods-devenv/scripts/restart-atlassian-suite-if-license-expires-in-less-than.sh
+++ ods-devenv/scripts/restart-atlassian-suite-if-license-expires-in-less-than.sh
@@ -8,6 +8,7 @@ echo " "
 # Initialize variables
 HOURS_LEFT=0
 HOURS_ATLASSIAN_CAN_BE_UP=3
+ALLOW_ZERO_HOURS="false"
 FORCE_RESTART="false";
 ASSUME_JUST_RESTARTED="false";
 LAST_RESTART_FILE_REGISTRY="/tmp/atlassian-suite-restarts-registry.log"
@@ -15,7 +16,7 @@ DEPLOY_SCRIPT="/home/openshift/opendevstack/ods-core/ods-devenv/scripts/deploy.s
 
 function usage() {
     echo " "
-    echo "${ME}: usage: ${ME} [--hours X] "
+    echo "${ME}: usage: ${ME} [--hours-left X] [--assume-just-restarted] [--force-restart] [--allow-zero-hours] "
     echo "${ME}: example: ${ME} --hours-left 1 "
     echo " "
 }
@@ -49,16 +50,34 @@ function checkIfWeStillHaveTime() {
     local MAX_HOURS_SINCE_LAST_UPDATE=$((HOURS_ATLASSIAN_CAN_BE_UP - HOURS_LEFT))
     echo "MAX_HOURS_SINCE_LAST_UPDATE=${MAX_HOURS_SINCE_LAST_UPDATE} (${HOURS_ATLASSIAN_CAN_BE_UP} - ${HOURS_LEFT})"
     
+    echo "Current date: $(date)"
     if [ "false" == ${FORCE_RESTART} ]; then
-        FOUND=find ${LAST_RESTART_FILE_REGISTRY} -mtime ${MAX_HOURS_SINCE_LAST_UPDATE}
-        echo "${ME}: FOUND: ${FOUND}"
+        FOUND=$(find ${LAST_RESTART_FILE_REGISTRY} -mtime -${MAX_HOURS_SINCE_LAST_UPDATE})
+        echo "Found tha file was modified in last ${MAX_HOURS_SINCE_LAST_UPDATE}h ?? (empty next line if no): "
+	echo "${FOUND}"
+	ls -lah ${LAST_RESTART_FILE_REGISTRY}
     fi
 
     if [ -z "${FOUND}" ] || [ "" == "${FOUND}" ]; then
-        echo "Restarting because file was not modified in last ${MAX_HOURS_SINCE_LAST_UPDATE}h."
+	if [ "true" == ${FORCE_RESTART} ]; then
+		echo " "
+		echo "Restart atlassian stack is forced !! "
+		echo " "
+	else
+		echo " "
+		echo "Since the registry file was *NOT* modified in the last ${MAX_HOURS_SINCE_LAST_UPDATE}h, "
+        	echo "we restart the atlassian stack to have at least ${HOURS_LEFT} until next license issue."
+		echo " "
+	fi
+
         ${DEPLOY_SCRIPT} --target restart_atlassian_suite
+	setCurrenTimeIfAssumeJustRestarted "true"
+	echo " "
     else
-        echo "Not restarting because the atlassian stack was up less than ${HOURS_LEFT}h."
+	echo " "
+        echo "Since the registry file was modified in the last ${MAX_HOURS_SINCE_LAST_UPDATE}h, "
+	echo "we do *NOT* need to restart the stack."
+	echo " "
     fi
 
 }
@@ -70,16 +89,18 @@ while [[ "$#" -gt 0 ]]; do
 
     -h|--help) usage; exit 0;;
 
-    --hours-left) HOURS_LEFT="$2"; echo "Hours until license expires: $HOURS_LEFT"; shift;;
+    --hours-left) HOURS_LEFT="$2"; echo "Hours we need the stack to not complaint about license issues: $HOURS_LEFT"; shift;;
 
     --assume-just-restarted) ASSUME_JUST_RESTARTED="true";;
 
     --force-restart) FORCE_RESTART="true";;
 
+    --allow-zero-hours) ALLOW_ZERO_HOURS="true";;
+
   *) echo_error "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-if [ 0 -eq ]; then
+if [ 0 -eq ${HOURS_LEFT} ] && [ "false" == "${ALLOW_ZERO_HOURS}" ] && [ "false" == "${ASSUME_JUST_RESTARTED}" ] && [ "false" == "${FORCE_RESTART}" ]; then
     usage
     echo " "
     echo "${ME}: Please provide the amount of hours you can wait for the stack to restart."
