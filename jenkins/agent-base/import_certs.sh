b742009bf279d433f49b1a4f3f3ad05fd792f725
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-09-05 16:12:37 +0200

    Setup correct permissions for cacerts folder.

--- jenkins/agent-base/import_certs.sh
+++ jenkins/agent-base/import_certs.sh
@@ -29,3 +29,11 @@ if [[ ! -z ${APP_DNS:=""} ]]; then
 else
     echo 'No certificates to import'
 fi
+
+echo "Trying to setup correct permissions for cacerts folder... "
+if [ ! -z "${JAVA_HOME}" ] && [ "" != "${JAVA_HOME}" ]; then
+    chmod -c g+w $JAVA_HOME/lib/security/cacerts
+else
+    echo "WARNING: Cannot apply permissions 'chmod g+w' to JAVA_HOME/lib/security/cacerts "
+    echo "WARNING: JAVA_HOME=${JAVA_HOME}"
+fi
