d07de06964b3a72e3cdd1a1c1af245c136d6fa7c
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-09-08 08:21:59 +0200

    Timeout=30h so if it hangs we have time to see why.

--- tests/verify.sh
+++ tests/verify.sh
@@ -15,8 +15,8 @@ fi
 if [ -f test-verify-results.txt ]; then
     rm test-verify-results.txt
 fi
-echo "go test -v -count=1 -timeout 10h github.com/opendevstack/ods-core/tests/ods-verify"
-go test -v -count=1 -timeout 10h github.com/opendevstack/ods-core/tests/ods-verify | tee test-verify-results.txt 2>&1
+echo "go test -v -count=1 -timeout 30h github.com/opendevstack/ods-core/tests/ods-verify"
+go test -v -count=1 -timeout 30h github.com/opendevstack/ods-core/tests/ods-verify | tee test-verify-results.txt 2>&1
 exitcode=$?
 if [ -f test-verify-results.txt ]; then
     set -e
