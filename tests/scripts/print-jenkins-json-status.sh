a2eed40c35718a649df952a81e32d6f710225471
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-09-12 14:44:47 +0200

    typo in comparison

--- tests/scripts/print-jenkins-json-status.sh
+++ tests/scripts/print-jenkins-json-status.sh
@@ -7,7 +7,7 @@ oc get build $1 -n $2 \
     -ojsonpath='{.metadata.annotations.openshift\.io/jenkins-status-json}' \
     | jq '[.stages[] | {stage: .name, status: .status}]' || OC_ERROR="true"
 
-if [ "false" == "${OC_ERROR}" ]; then
+if [ "false" != "${OC_ERROR}" ]; then
     echo " "
     echo "ERROR: Could not get oc build status named $1 in namespace $2 !!! "
     echo " "
