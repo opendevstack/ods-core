e3d5505e0cb6e04d83f7f8ba980a1c0552ac3266
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2023-02-09 17:19:16 +0100

    centos and ODSbox removal, initial clean

--- ods-devenv/packer/pwd_sudo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# give wheel their pwd back
-sudo chattr -i /etc/sudoers
-sudo sed -i '0,/%wheel[[:space:]]*ALL=(ALL)[[:space:]]*NOPASSWD:[[:space:]]*ALL/{s||%wheel  ALL=(ALL)       ALL|}' /etc/sudoers
