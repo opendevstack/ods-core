87d2f4e8d3dd608732eed7f564b0d27a0be84c5d
zxBCN Sevillano,Sergio (IT EDS) EXTERNAL <sevillan@WKS0001129428>
2023-05-26 15:54:20 +0200

    jenkins master and base updated

--- jenkins/master/configuration/init.groovy.d/sonarqube.groovy
+++ jenkins/master/configuration/init.groovy.d/sonarqube.groovy
@@ -1,29 +1,35 @@
 import jenkins.model.*
 import hudson.plugins.sonar.*
 import hudson.plugins.sonar.model.*
-import hudson.plugins.sonar.utils.SQServerVersions
+import hudson.util.Secret
+import org.jenkinsci.plugins.plaincredentials.StringCredentials
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
+import com.cloudbees.plugins.credentials.CredentialsScope
+import org.apache.commons.lang.StringUtils;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
 
 def jen = Jenkins.getInstance()
 def env = System.getenv()
+def secret = Secret.fromString(env['SONAR_SERVER_AUTH_TOKEN'])
 
-// https://github.com/SonarSource/sonar-scanner-jenkins/blob/sonar-2.6/src/main/java/hudson/plugins/sonar/SonarGlobalConfiguration.java
+StringCredentials cred = new StringCredentialsImpl(CredentialsScope.GLOBAL, "sonar-token", null, secret);
+SystemCredentialsProvider instance = SystemCredentialsProvider.getInstance()
+instance.getCredentials().add(cred)
+instance.save()
+
+
 def conf = jen.getDescriptor("hudson.plugins.sonar.SonarGlobalConfiguration")
 
-// https://github.com/SonarSource/sonar-scanner-jenkins/blob/sonar-2.6/src/main/java/hudson/plugins/sonar/SonarInstallation.java
 def inst = new SonarInstallation(
   "SonarServerConfig",
   env['SONAR_SERVER_URL'],
-  SQServerVersions.SQ_5_3_OR_HIGHER,
-  env['SONAR_SERVER_AUTH_TOKEN'],
-  "",
-  "",
-  "",
-  "",
-  "",
-  new TriggersConfig(),
-  "",
-  "",
-  ""
+  'sonar-token',
+  secret,
+  null,
+  null,
+  null,
+  null,
+  new TriggersConfig()  
 )
 
 conf.setInstallations(inst)
