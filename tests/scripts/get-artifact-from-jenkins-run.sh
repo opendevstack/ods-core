c6ffa9d39beb162f3b1487fba1ae5a7a647d2a69
Jorge Romero <jorge.romero.ext@boehringer-ingelheim.com>
2023-06-28 16:13:14 +0200

    Change the way we get the token to interact with jenkins

--- tests/scripts/get-artifact-from-jenkins-run.sh
+++ tests/scripts/get-artifact-from-jenkins-run.sh
@@ -8,7 +8,7 @@ BUILD_URL=$(oc get -n ${PROJECT} build ${BUILD_NAME} -o jsonpath='{.metadata.ann
 echo $BUILD_URL
 ARTIFACT_URL=$BUILD_URL/artifact/artifacts/$3
 echo "grabbing artifact from $ARTIFACT_URL - and storing in /tmp"
-TOKEN=$(oc -n ${PROJECT} get sa/jenkins --template='{{range .secrets}}{{ .name }} {{end}}' | xargs -n 1 oc -n ${PROJECT} get secret --template='{{ if .data.token }}{{ .data.token }}{{end}}' | head -n 1 | base64 -d -)
+TOKEN=$(oc whoami --show-token)
 httpCode=$(curl --insecure -sS ${ARTIFACT_URL} --header "Authorization: Bearer ${TOKEN}" -o /tmp/$3 -w "%{http_code}")
 echo "response: $httpCode"
 if [ ! "${httpCode}" == "200" ]; then
