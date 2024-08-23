ae23cb14a0ad714be0bf8830ca5a3769019b18c4
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2024-08-23 12:29:59 +0200

    set default java version

--- jenkins/agent-base/fix_java_certs_permissions.sh
+++ jenkins/agent-base/fix_java_certs_permissions.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -eu
 
-# Initialize JAVA_HOME and set alternatives.
-NO_JAVA_LINK="false"
-java -version || NO_JAVA_LINK="true"
-if [ "true" == "${NO_JAVA_LINK}" ]; then
-    JAVA_HOME_FOLDER=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-.*\.x86_64" | awk '{print $NF}' | head -1)
-    JAVA_HOME="/usr/lib/jvm/${JAVA_HOME_FOLDER}"
-    alternatives --set java ${JAVA_HOME}/bin/java
+# Initialize JAVA_HOME if not set.
+JAVA_HOME=${JAVA_HOME:-""}
+
+if [ -f /etc/profile.d/set-default-java.sh ]; then
+    source /etc/profile.d/set-default-java.sh
+else
+    echo "WARNING: Not setting default java version."
 fi
-java -version
 
 echo "Trying to setup correct permissions for cacerts folder... "
 if [ ! -z "${JAVA_HOME}" ] && [ "" != "${JAVA_HOME}" ]; then
