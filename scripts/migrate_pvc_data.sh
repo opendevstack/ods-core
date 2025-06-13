85e07cd6ac338f117873527506aad3743e59768c
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2025-06-13 15:03:43 +0200

    add new params and rework migration comand by adding threads

--- scripts/migrate_pvc_data.sh
+++ scripts/migrate_pvc_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exit on error is removed to allow the script to continue on failure
-# set -e
-
 # Function to display usage
 usage() {
   printf "Migrate data from one PVC to another within the OpenShift cluster.\n\n"
@@ -13,6 +10,8 @@ usage() {
   printf "\t-s|--source-pvc\t\tName of the source PVC\n"
   printf "\t-t|--target-pvc\t\tName of the target PVC\n"
   printf "\t-n|--namespace\t\tNamespace where the PVCs are located\n"
+  printf "\t-p|--threads\t\tNumber of parallel threads (default: 5)\n"
+  printf "\t-c|--cpu\t\tNumber of CPU cores to request (default: 1)\n"
 }
 
 # Parse arguments
@@ -26,10 +25,23 @@ while [[ "$#" -gt 0 ]]; do
     -t=*|--target-pvc=*) TARGET_PVC="${1#*=}" ;;
     -n|--namespace) NAMESPACE="$2"; shift ;;
     -n=*|--namespace=*) NAMESPACE="${1#*=}" ;;
+    -p|--threads) THREADS="$2"; shift ;;
+    -p=*|--threads=*) THREADS="${1#*=}" ;;
+    -c|--cpu) CPU_REQUEST="$2"; shift ;;
+    -c=*|--cpu=*) CPU_REQUEST="${1#*=}" ;;
+    --cpu) CPU_REQUEST="$2"; shift ;;
+    --cpu=*) CPU_REQUEST="${1#*=}" ;;
     *) echo "Unknown parameter passed: $1"; usage; exit 1 ;;
   esac
   shift
 done
+
+# Set default threads and cpu if not provided
+THREADS="${THREADS:-5}"
+CPU_REQUEST="${CPU_REQUEST:-1}"
+
+# Calculate CPU limit (always 2 more than request)
+CPU_LIMIT=$((CPU_REQUEST + 2))
 
 # Validate arguments
 if [[ -z "$SOURCE_PVC" || -z "$TARGET_PVC" || -z "$NAMESPACE" ]]; then
@@ -50,14 +62,31 @@ spec:
   containers:
   - name: rsync-container
     image: image-registry.openshift-image-registry.svc:5000/openshift/tools
-    command: ["/bin/sh", "-c", "time rsync -avh --omit-dir-times --stats --human-readable --info=progress2 --partial --ignore-errors /tmp/source/ /tmp/target/; while true; do sleep 3600; done"]
+    command:
+      - /bin/sh
+      - -c
+      - |
+        start_time=\$(date +%s)
+        find /tmp/source -type f -print0 | xargs -0 -n1 -P${THREADS} -I{} sh -c '
+          target_dir="/tmp/target/\$(dirname "{}" | sed "s|/tmp/source||")"
+          mkdir -p "\$target_dir"
+          rsync -avh --omit-dir-times --stats --human-readable --info=progress2 --partial --ignore-errors "{}" "\$target_dir/"
+        '
+        end_time=\$(date +%s)
+        duration=\$((end_time - start_time))
+        duration_min=\$((duration / 60))
+        duration_hr=\$((duration / 3600))
+        echo "Data migration completed successfully."
+        echo "Time taken: \${duration} seconds (\${duration_min} minutes, \${duration_hr} hours)."
+        echo "You can now safely remove the migration pod."
+        while true; do sleep 3600; done
     resources:
       requests:
         memory: "2Gi"
-        cpu: "250m"
+        cpu: "${CPU_REQUEST}"
       limits:
         memory: "2Gi"
-        cpu: '2'
+        cpu: '${CPU_LIMIT}'
     volumeMounts:
     - name: source-pvc
       mountPath: /tmp/source
