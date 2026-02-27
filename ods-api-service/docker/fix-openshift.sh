5f0716e8561a2fe14ba5d8cb668d985ad14b6943
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2026-02-27 14:22:48 +0100

    test

--- ods-api-service/docker/fix-openshift.sh
+++ ods-api-service/docker/fix-openshift.sh
@@ -6,9 +6,8 @@ export PGUSER=postgres
 export POSTGRES_USER=postgres
 
 # Prevent entrypoint from trying to chmod /var/run/postgresql
-# by creating it with proper perms first
 mkdir -p /var/run/postgresql
-chmod 775 /var/run/p
+chmod 775 /var/run/postgresql
 
 # Execute the real Postgres entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
