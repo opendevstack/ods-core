9bc7848e81c8977c3464e2e2066ba86d512a7c81
Juan Farré <13270880+jafarre-bi@users.noreply.github.com>
2026-08-28 10:51:24 +0200

    Fix tests

--- create-projects/tests/run.sh
+++ create-projects/tests/run.sh
@@ -97,7 +97,7 @@ echo "=== create-cd-jenkins: With general CD user ==="
 
 tailor mock --receive='version' --stdout='1.3.4'
 
-tailor mock --receive='--non-interactive apply --namespace=foo-cd --param=PIPELINE_TRIGGER_SECRET_B64=czNjcjN0 --param=WEBHOOK_HMAC_SECRET=d2ViaG9vay1zZWNyZXQ= --param=WEBHOOK_ALLOWED_IP_RANGES=0.0.0.0/0 --param=PROJECT=foo --param=CD_USER_ID_B64=Y2RfdXNlcg== --param=ODS_NAMESPACE=bar --param=ODS_IMAGE_TAG=3.x --param=ODS_BITBUCKET_PROJECT=opendevstack --selector template=ods-jenkins-template' --times 1
+tailor mock --receive='--non-interactive apply --namespace=foo-cd --param=PIPELINE_TRIGGER_SECRET_B64=czNjcjN0 --param=WEBHOOK_HMAC_KEY_B64=d2ViaG9vay1zZWNyZXQ= --param=WEBHOOK_ALLOWED_IP_RANGES=0.0.0.0/0 --param=PROJECT=foo --param=CD_USER_ID_B64=Y2RfdXNlcg== --param=ODS_NAMESPACE=bar --param=ODS_IMAGE_TAG=3.x --param=ODS_BITBUCKET_PROJECT=opendevstack --selector template=ods-jenkins-template' --times 1
 
 ../create-cd-jenkins.sh \
     --project foo \
@@ -118,7 +118,7 @@ echo "=== create-cd-jenkins: With project-specific CD user ==="
 
 tailor mock --receive='version' --stdout='1.3.4'
 
-tailor mock --receive='--non-interactive apply --namespace=foo-cd --param=PIPELINE_TRIGGER_SECRET_B64=czNjcjN0 --param=WEBHOOK_HMAC_SECRET=d2ViaG9vay1zZWNyZXQ= --param=WEBHOOK_ALLOWED_IP_RANGES=0.0.0.0/0 --param=PROJECT=foo --param=CD_USER_ID_B64=Zm9v --param=ODS_NAMESPACE=bar --param=ODS_IMAGE_TAG=3.x --param=ODS_BITBUCKET_PROJECT=opendevstack --param=CD_USER_PWD_B64=Y2hhbmdlbWU= --selector template=ods-jenkins-template' --times 1
+tailor mock --receive='--non-interactive apply --namespace=foo-cd --param=PIPELINE_TRIGGER_SECRET_B64=czNjcjN0 --param=WEBHOOK_HMAC_KEY_B64=d2ViaG9vay1zZWNyZXQ= --param=WEBHOOK_ALLOWED_IP_RANGES=0.0.0.0/0 --param=PROJECT=foo --param=CD_USER_ID_B64=Zm9v --param=ODS_NAMESPACE=bar --param=ODS_IMAGE_TAG=3.x --param=ODS_BITBUCKET_PROJECT=opendevstack --param=CD_USER_PWD_B64=Y2hhbmdlbWU= --selector template=ods-jenkins-template' --times 1
 
 ../create-cd-jenkins.sh \
     --project foo \
